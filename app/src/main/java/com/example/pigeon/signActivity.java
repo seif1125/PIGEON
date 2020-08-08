c8fa96d091481d8071edbd45eb87b6570dbdfd2e
seif1125 <57404468+seif1125@users.noreply.github.com>
2020-08-08 02:20:14 +0200

    finishing signup activity && bckend building api (.php)

---
+++ app/src/main/java/com/example/pigeon/signActivity.java
@@ -0,0 +1,14 @@
+package com.example.pigeon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+    }
+}
